c648bd183b75e65a4695f281eb6d7749f1021bd7
Alain <alainman.krh@gmail.com>
2024-02-03 21:15:51 -0500

    First implementation of sequences.

--- libpython/AVglue/Actions.py
+++ libpython/AVglue/Actions.py
@@ -2,28 +2,42 @@
 #-------------------------------------------------------------------------------
 from .Base import AbstractAction
 from os import system
+import ctypes
 
 
 #==Concrete Actions
 #===============================================================================
 class Action_TriggerLocalSignal(AbstractAction):
 	"""Trigger a signal that gets processed locally"""
-	def __init__(self, sigid):
-		self.sigid = sigid
+	def __init__(self, signame):
+		self.signame = signame
 
 	def serialize(self):
-		return f"TRIGLCL {self.sigid}"
+		return f"TRIGLCL {self.signame}"
 
 class Action_TriggerComSignal(AbstractAction):
 	"""Trigger a signal that gets sent through a com device"""
-	def __init__(self, sigid):
-		self.sigid = sigid
+	def __init__(self, signame):
+		self.signame = signame
 
 	def serialize(self):
-		return f"TRIGCOM {self.sigid}"
+		return f"TRIGCOM {self.signame}"
 
-class Action_SendKey(AbstractAction):
-	pass
+class Action_SendKeys(AbstractAction):
+	"""Sends a key sequence"""
+	#TODO: Send to a particular application???
+	def __init__(self, wnd, seq):
+		"""-wnd=0 sends key sequence to active window"""
+		self.wnd = wnd
+		self.seq = seq
+
+	def run(self):
+		print("Sending:", self.seq)
+		result = ctypes.windll.user32.SendInput(self.seq)
+		return result
+
+	def serialize(self):
+		return f"SENDKEYS {self.wnd} {self.seq}"
 
 class Action_ExecuteShell(AbstractAction):
 	"""Execute shell - not necessarily portable"""
@@ -48,3 +62,7 @@ class Action_ExecuteSequence(AbstractAction):
 	"""Sequential list of actions"""
 	def __init__(self, action_list):
 		self.action_list = action_list #::AbstractAction[]
+
+	def run(self):
+		for action in self.action_list:
+			action.run()
--- samples/basictests.py
+++ samples/basictests.py
@@ -1,4 +1,11 @@
 from AVglue.Actions import *
 
 act_calcopen = Action_ExecuteShell("calc.exe")
-act_calcopen.run()
+#act_calcopen.run()
+
+
+act_dothething = Action_ExecuteSequence([
+    #Action_ExecuteShell("calc.exe"),
+    Action_SendKeys(0, "3.14"),
+])
+act_dothething.run()
