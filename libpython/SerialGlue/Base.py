1f4170b72ae704b53aa488f61505a8e1ac6c9bb9
Alain <alainman.krh@gmail.com>
2024-02-17 16:09:19 -0500

    PortManager

--- libpython/SerialGlue/Base.py
+++ libpython/SerialGlue/Base.py
@@ -1,30 +1,48 @@
 #SerialGlue/Base.py
 #-------------------------------------------------------------------------------
 import os
+from copy import copy
 
 if os.name == 'nt':  # sys.platform == 'win32':
-    from serial.tools.list_ports_windows import comports
+	from serial.tools.list_ports_windows import comports
 elif os.name == 'posix':
-    from serial.tools.list_ports_posix import comports
+	from serial.tools.list_ports_posix import comports
 else:
-    raise ImportError("OS not supported: {os.name}.")
+	raise ImportError("OS not supported: {os.name}.")
 
 
-#==comport_find
+#==PortManager
 #===============================================================================
-def comport_find(sn:str):
-	sn = sn.lower()
-	iterator = sorted(comports())
+class PortManager:
+	def __init__(self) -> None:
+		self.port_list = [] #List of port_info=(port, desc, hwid)
+	
+	def portlist_refresh(self):
+		#Copy port_info - just in case iterator doesn't return copies itself
+		iterator = sorted(comports())
+		self.port_list = tuple(copy(port_info) for port_info in iterator)
 
-	for n, (port, desc, hwid) in enumerate(iterator, 1):
-		hwid_split = hwid.split()
-		for v in hwid_split:
-			if "SER=" in v:
-				sn_i = v.split("=")[-1]
-				if sn_i.lower() == sn:
-					return port
-	return None
+	def portlist_diplay(self, refresh=True):
+		if refresh:
+			self.portlist_refresh()
+		for (port, desc, hwid) in self.port_list:
+			print(f"{port:20}")
+			print(f"    desc: {desc}")
+			print(f"    hwid: {hwid}")
+
+	def portid_fromserialno(self, sn):
+		sn = sn.lower()
+		for (port, desc, hwid) in self.port_list:
+			hwid_split = hwid.split()
+			for v in hwid_split:
+				if "SER=" in v:
+					sn_i = v.split("=")[-1]
+					if sn_i.lower() == sn:
+						return port
+		return None
 
 sn = "SOMESERIALNO"
-port = comport_find(sn)
-print(port)
+mgr = PortManager()
+mgr.portlist_diplay()
+portid = mgr.portid_fromserialno(sn)
+print(portid)
